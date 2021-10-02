b1aac9a1421e26ba4bb00afd9ca2e7f19ecef29e
Szilveszter Dávid <szilveszterdavid@yahoo.com>
2021-10-02 21:31:28 +0300

    Displaying ingredient portions

--- Recipe List App/ViewModels/RecipeModel.swift
+++ Recipe List App/ViewModels/RecipeModel.swift
@@ -20,16 +20,70 @@ class RecipeModel: ObservableObject {
     // we can call without creating an instance
     static func getPortion(ingredient: Ingredients, recipeServings: Int, targetServings: Int) -> String {
         
-        // Get a single serving size by multiplying denominator by trhe recipe servings
+        var portion = ""
+        var numerator = ingredient.num ?? 1
+        var denominator = ingredient.denom ?? 1
+        var wholePortions = 0
         
-        // Get a target portion by multyplying numerator by target service
+        if ingredient.num != nil {
+         
+            // Get a single serving size by multiplying denominator by trhe recipe servings
+            denominator *= recipeServings
+            
+            // Get a target portion by multyplying numerator by target service
+            numerator *= targetServings
+            
+            // Reduce fraction by greatest common divisor
+            let divisor = Rational.greatestCommonDivisor(numerator, denominator)
+            numerator /= divisor
+            denominator /= divisor
+            
+            // Get the whole portion if numerator > denominator
+            if numerator >= denominator {
+                
+                // Calculated whole portions
+                wholePortions = numerator / denominator
+                
+                // Calculate the remainder
+                numerator = numerator % denominator
+                
+                // Assign to portion string
+                portion += String(wholePortions)
+            }
+            
+            // Express the remainder as a fraction
+            if numerator > 0 {
+                
+                // Assign remainder as fraction to the portion string
+                portion += wholePortions > 0 ? " " : ""
+                portion += "\(numerator)/\(denominator)"
+            }
+        }
         
-        // Reduce fraction by greatest common divisor
+        if var unit = ingredient.unit {
+            
+            var suffix = ""
+            
+            if wholePortions > 1 {
+                
+                // Calculate appropiate suffix
+                if unit.suffix(2) == "ch" {
+                    unit += "es"
+                }
+                else if unit.suffix(1) == "f" {
+                    unit = String(unit.dropLast())
+                    suffix += "ves"
+                }
+                else {
+                    unit += "s"
+                }
+            }
+            
+            portion += ingredient.num == nil && ingredient.denom == nil ? "" : " "
+            
+            return portion + unit
+        }
         
-        // Get the whole portion if numerator > denominator
-        
-        // Express the remainder as a fraction
-        
-        return String(targetServings)
+        return portion
     }
 }
--- Recipe List App/Views/RecipeDetailView.swift
+++ Recipe List App/Views/RecipeDetailView.swift
@@ -44,7 +44,7 @@ struct RecipeDetailView: View {
                         .padding([.bottom, .top], 5)
                     
                     ForEach(recipe.ingredients) { item in
-                        Text("• " + RecipeModel.getPortion(ingredient: item, recipeServings: recipe.servings, targetServings: selectedServingSize) + " " + item.name)
+                        Text("• " + RecipeModel.getPortion(ingredient: item, recipeServings: recipe.servings, targetServings: selectedServingSize) + " " + item.name.lowercased())
                     }
                 }
                 .padding(.horizontal)
