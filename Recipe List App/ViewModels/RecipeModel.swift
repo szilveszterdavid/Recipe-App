f1b7e2b49b1bf9859b306fc0a36cb163494b4962
Szilveszter Dávid <szilveszterdavid@yahoo.com>
2021-10-02 19:45:41 +0300

    Picker

--- Recipe List App/ViewModels/RecipeModel.swift
+++ Recipe List App/ViewModels/RecipeModel.swift
@@ -16,4 +16,20 @@ class RecipeModel: ObservableObject {
         // Create an instance of data service and get the data
         self.recipes = DataService.getLocalData() // because getlocalData is a static function
     }
+    
+    // we can call without creating an instance
+    static func getPortion(ingredient: Ingredients, recipeServings: Int, targetServings: Int) -> String {
+        
+        // Get a single serving size by multiplying denominator by trhe recipe servings
+        
+        // Get a target portion by multyplying numerator by target service
+        
+        // Reduce fraction by greatest common divisor
+        
+        // Get the whole portion if numerator > denominator
+        
+        // Express the remainder as a fraction
+        
+        return String(targetServings)
+    }
 }
